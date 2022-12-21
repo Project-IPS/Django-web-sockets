cbf1a51f2188c484818fb5247da5dc298b902fd8
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-12-21 12:28:31 +0530

    added for loop to send msgs continuously

--- tmdb_app/websockets/consumers.py
+++ tmdb_app/websockets/consumers.py
@@ -2,56 +2,67 @@ from channels.consumer import SyncConsumer
 from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
 import json
+from time import sleep
+import asyncio
+
 
 class MySyncConsumer(SyncConsumer):
 
     def websocket_connect(self, event):
         print("websocket Connected....", event)
         self.send({
-            'type' : 'websocket.accept'
+            'type': 'websocket.accept'
         })
 
     def websocket_receive(self, event):
         print("Websocket Received...")
         print("Message is ", event['text'])
-        self.send({
-            'type': "websocket.send",
-            "text": "msg from server to the client"   # this will be sent to client...
+        for i in range(50):
+            sleep(0.5)
+            self.send({
+                'type': "websocket.send",
+                # this will be sent to client...
+                "text": str(i)+". msg from server to the client"
             })
-
 
     def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
         raise StopConsumer()
+
 
 class MyAsyncConsumer(AsyncConsumer):
 
     async def websocket_connect(self, event):
         print("websocket Connected....", event)
         await self.send({
-            'type' : 'websocket.accept'
+            'type': 'websocket.accept'
         })
 
     async def websocket_receive(self, event):
         print("Websocket Received...")
         print("Message is ", event['text'])
-        
 
-        json_obj = {
-            'field_1': 1,
-            'field_2': 2,
-            'id': 4,
-            'fname': "Balbir",
-            "lname":"Yadav"
-        }
+        # json_obj = {
+        #     'field_1': 1,
+        #     'field_2': 2,
+        #     'id': 4,
+        #     'fname': "Balbir",
+        #     "lname": "Yadav"
+        # }
 
-        await self.send({
-            "type": "websocket.send",
-            "text": json.dumps(json_obj)
-        })
+        # await self.send({
+        #     "type": "websocket.send",
+        #     "text": json.dumps(json_obj)
+        # })
+
+        for i in range(50):
+            await asyncio.sleep(0.5)
+            await self.send({
+                'type': "websocket.send",
+                # this will be sent to client...
+                "text": str(i)+". msg from server to the client"
+            })
 
     async def websocket_disconnect(self, event):
         print("Websocket Disconnected...", event)
         raise StopConsumer()
-
-    
